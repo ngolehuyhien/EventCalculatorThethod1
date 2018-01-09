d4c9ddf6f9f83b8e9c4c42e43c2bbb976482e7f3
ngolehuyhien <hien.ngo160203@vnuk.edu.vn>
2018-01-09 09:30:37 +0700

    Submit

--- app/src/main/java/com/example/admin/eventcalculator/MainActivity.java
+++ app/src/main/java/com/example/admin/eventcalculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.admin.eventcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,21 +15,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onClickAddButton(View view) {
-        EditText edtResult = (EditText) findViewById(R.id.edt_result);
-        EditText numberA = (EditText) findViewById(R.id.input_a);
-        EditText numberB = (EditText) findViewById(R.id.input_b);
+    final Button button = (Button) findViewById(R.id.edt_result);
+    button.setOnClickListener(new View.OnClickListener() {
 
-        if(!numberA.getText().toString().trim().equals("") && !numberB.getText().toString().trim().equals(""))  {
+        public void onClick(View view) {
+            EditText edtResult = (EditText) findViewById(R.id.edt_result);
+            EditText numberA = (EditText) findViewById(R.id.input_a);
+            EditText numberB = (EditText) findViewById(R.id.input_b);
 
-            int a = Integer.parseInt(numberA.getText().toString());
-            int b = Integer.parseInt(numberB.getText().toString());
-            int sum = a + b;
-            edtResult.setText(String.valueOf(sum));
+            if(!numberA.getText().toString().trim().equals("") && !numberB.getText().toString().trim().equals(""))  {
+
+                int a = Integer.parseInt(numberA.getText().toString());
+                int b = Integer.parseInt(numberB.getText().toString());
+                int sum = a + b;
+                edtResult.setText(String.valueOf(sum));
+            }
+            else
+            {
+                Toast.makeText(this, "Vui lòng nhập số vào!", Toast.LENGTH_SHORT).show();
+            }
         }
-        else
-        {
-            Toast.makeText(this, "Vui lòng nhập số vào!", Toast.LENGTH_SHORT).show();
-        }
-    }
+    });
 }
